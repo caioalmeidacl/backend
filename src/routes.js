a5e00a0d83234aabcdcd53f6a2d24e32d840dda4
Caio Almeida <caioalmeidaacl@gmail.com>
2021-03-31 04:08:44 -0300

    Before calculating the date

--- src/routes.js
+++ src/routes.js
@@ -5,18 +5,29 @@ const views = __dirname + "/views/"
 
 const profile = {
     name: "Caio",
-    avatar: "https://avatars.githubusercontent.com/u/7559318?v=4",
+    avatar: "https://github.com/caioalmeidacl.png",
     "monthly-budget": 3000,
     "days-per-week": 5,
     "hours-per-day": 5,
     "vacation-per-year": 3
 }
 
+const jobs = []
+
 // request, response
-routes.get('/', (req, res) =>  res.render(views + "/index"))
-routes.get('/job', (req, res) =>  res.render(views + "/job"))
-routes.get('/job/edit', (req, res) =>  res.render(views + "/job-edit"))
-routes.get('/profile', (req, res) =>  res.render(views + "/profile", {profile}))
+routes.get('/', (req, res) =>  res.render(views + "index"))
+routes.get('/job', (req, res) =>  res.render(views + "job"))
+routes.post('/job', (req, res) => {
+  // req.body = {name: 'caio', 'daily-hours': '12 , 'total-hours': '10'}
+
+    const job = req.body
+    job.createdAt = new Date.now() // Atribuindo uma nova data
+
+  jobs.push(req.body)
+  return res.redirect('/')
+})
+routes.get('/job/edit', (req, res) =>  res.render(views + "job-edit"))
+routes.get('/profile', (req, res) =>  res.render(views + "profile", {profile}))
 
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ server.set('view engine', 'ejs')
 //habilitar arquivos statics
 server.use(express.static("public"))
 
+// usar req.body
+server.use(express.urlencoded({ extended: true }))
+
+
 //routes
 server.use(routes)
 server.listen(3000, () => console.log('rodando'))
